66721fab1fa6ad4c3f7349421af9b4b2126edb41
cccmark <cccmarcos@gmail.com>
2021-04-29 13:13:14 -0500

    test for the comments on python file

--- w3resource Python exercises/ex1.py
+++ w3resource Python exercises/ex1.py
@@ -1,7 +1,7 @@
-def max_of_two (x,y):
-	if x>y:
-		return x
-	return y
-def max_of_three(x,y,z):
-	return max_of_two (x, max_of_two (y,z))
-print(max_of_three(3, 6, -5))
+def max_of_two (x,y): ##defines max of 2 numbers
+	if x>y: ##if x is bigger than y
+		return x ## return x
+	return y ##else return y
+def max_of_three(x,y,z):  ##define max of 3 numbers
+	return max_of_two (x, max_of_two (y,z)) ##return the max of the 2 and x
+print(max_of_three(3, 6, -5)) ## prints the max of 3 numbers example
